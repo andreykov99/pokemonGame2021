dc97648e4a7afc5bcd8062b8af5c633d948312a9
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2021-03-11 19:41:01 +0200

    add props to layouts

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import './App.css';
 import Header from './components/Header/Header';
 import Layout from './components/Layout/Layout';
 import Footer from './components/Footer/Footer';
+import firstImage from '../src/assets/bg1.jpg';
+import secondImage from '../src/assets/bg2.jpg';
 
 function App() {
   return (
     <>
-      <Header />
-      <Layout />
-      <Layout />
-      <Layout />
+      <Header title='' desc='' />
+      <Layout
+        title='This is title'
+        desc='This is Description!'
+        urlBg={firstImage} />
+      <Layout
+        title='This is title'
+        desc='This is Description!'
+        colorBg='#ffae42' />
+      <Layout
+        title='This is title'
+        desc='This is Description!'
+        urlBg={secondImage} />
       <Footer />
     </>
   );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Layout.module.css';
 
+
 const Layout = (props) => {
+    const bg = { background: (props.urlBg) ? `url(${props.urlBg}) 0 0/cover` : props.colorBg }
     return (
-        <section className={styles.root}>
+        <section className={styles.root} style={bg}>
             <div className={styles.wrapper}>
-                <article>
+                <article >
                     <div className={styles.title}>
                         <h3>{props.title}</h3>
                         <span className={styles.separator}></span>
