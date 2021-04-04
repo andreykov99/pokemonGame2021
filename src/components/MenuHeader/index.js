b1fd83ce9ced9a135523ff38fee046e5f6ef8152
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2021-04-04 14:19:54 +0300

    add modal component

--- src/components/MenuHeader/index.js
+++ src/components/MenuHeader/index.js
@@ -1,18 +1,39 @@
 import { useState } from 'react';
 import Navbar from '../Navbar';
 import Menu from '../Menu';
+import Modal from '../Modal';
 
 
 const MenuHeader = ({ bgActive }) => {
     const [isActive, setActive] = useState(null);
+    const [isModalOpen, setModalOpen] = useState(null);
     const handleMenuButtonChange = () => {
         setActive(prevState => !prevState);
     }
 
+    const handleClickLogin = () => {
+        setModalOpen(prevState => !prevState);
+    }
+
     return (
         <>
-            <Navbar onMenuButtonChange={handleMenuButtonChange} isActive={isActive} bgActive={bgActive} />
-            <Menu isActive={isActive} closeMenu={handleMenuButtonChange} />
+            <Navbar
+                onMenuButtonChange={handleMenuButtonChange}
+                onClickLogin={handleClickLogin}
+                isActive={isActive}
+                bgActive={bgActive}
+            />
+            <Menu
+                isActive={isActive}
+                closeMenu={handleMenuButtonChange}
+            />
+            <Modal
+                isOpen={isModalOpen}
+                title="This is title"
+                onCloseModal={handleClickLogin}
+            >
+                Some text here...
+            </Modal>
         </>
     )
 }
