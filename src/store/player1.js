a427291639ffe0b5a77bf97ba68d44f83a389a92
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2021-04-04 12:29:37 +0300

    add game slice to store

---
+++ src/store/player1.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const slice = createSlice({
+
+    name: 'player1',
+    initialState: {
+        cards: [],
+    },
+    reducers: {
+
+    }
+});
