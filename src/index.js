eb550b01bef4f0b912bd32437275e36a10ff7304
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-08-24 19:51:26 +0300

    update packages

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from './store';
 
 import App from './App';
 import './services/firebase';
 import './index.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
