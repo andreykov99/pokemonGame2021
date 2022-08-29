3c90ffeedbfed09e8ed2037c0dc0192ab631e7cf
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-08-29 19:18:09 +0300

    ref: update GamePage

--- src/routes/Game/GamePage.js
+++ src/routes/Game/GamePage.js
@@ -1,14 +1,7 @@
 import { useState } from 'react';
-import { useRouteMatch, Route, Switch } from 'react-router-dom';
-
-import PokemonsContext from '../../context/PokemonsContext';
-
-import StartPage from './routes/Start';
-import BoardPage from './routes/Board';
-import FinishPage from './routes/Finish';
+import { Outlet } from 'react-router-dom';
 
 const GamePage = () => {
-  const match = useRouteMatch();
   const [selectedPokemons, setSelectedPokemons] = useState({});
   const [player2Pokemons, setPlayer2Pokemons] = useState({});
 
@@ -30,23 +23,7 @@ const GamePage = () => {
     setSelectedPokemons({});
   };
 
-  return (
-    <PokemonsContext.Provider
-      value={{
-        pokemons: selectedPokemons,
-        onSelectedPokemons: handleSelectedPokemons,
-        clearSelectedPokemons: handleClearSelected,
-        pokemons2: player2Pokemons,
-        setPokemons2: setPlayer2Pokemons,
-      }}
-    >
-      <Switch>
-        <Route path={`${match.path}/`} exact component={StartPage} />
-        <Route path={`${match.path}/board`} component={BoardPage} />
-        <Route path={`${match.path}/finish`} component={FinishPage} />
-      </Switch>
-    </PokemonsContext.Provider>
-  );
+  return <Outlet />;
 };
 
-export default GamePage;
+export { GamePage };
